6850a1a4af0370cbe8385c17c04527555e5ef7be
Yvem <481983+Yvem@users.noreply.github.com>
2025-05-27 08:27:34 +1000

    +++

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hello-world--cloudflare--2025
+
 Testing Cloudflare in 2025
+
+
+https://hello-world--cloudflare--2025.pages.dev/
+
+https://dash.cloudflare.com/b2af5783377fe245a6520d902507e050/pages/view/hello-world--cloudflare--2025
--- functions/hello-world.ts
+++ functions/hello-world.ts
@@ -1,4 +1,9 @@
 // https://developers.cloudflare.com/pages/functions/get-started/#create-a-function
 export function onRequest(context) {
+	console.log('(console.log)', context)
+	console.info('(console.info)', context)
+	console.warn('(console.warn)', context)
+	console.error('(console.error)', context)
+
 	return new Response('Hello, world!')
 }
